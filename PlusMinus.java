51f70f8fdef1c72d5951e2f2f0ce95479e3cf27d
Ananda Zuraenee Sudarmaji <97554212+zuraenee@users.noreply.github.com>
2023-12-03 03:59:44 +0700

    Create PlusMinus.java

---
+++ PlusMinus.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<Integer> arr) {
+    // Write your code here
+    int totalSize = arr.size();
+        double PositiveNumber = 0;
+        double NegativeNumber = 0;
+        double Zero = 0;
+        // Write your code here
+        for (int i=0; i<totalSize; i++ ){
+            if (arr.get(i)>0){
+                PositiveNumber += 1;
+            }
+            else if (arr.get(i)<0){
+                NegativeNumber += 1;
+            }
+            else if (arr.get(i) == 0){
+                Zero += 1;
+            }
+        }
+            System.out.println(String.format("%.6f",PositiveNumber/totalSize));
+            System.out.println(String.format("%.6f",NegativeNumber/totalSize));
+            System.out.println(String.format("%.6f",Zero/totalSize));
+        
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.plusMinus(arr);
+
+        bufferedReader.close();
+    }
+}
